5f3a2d668301b4f5261aed0b1943bb5b2f6e2bf0
maciekto <ekstar007@gmail.com>
2021-04-26 19:52:21 +0200

    Add scss. Start create navBar_auth.

---
+++ src/components/2_layouts/Nav_auth.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+
+// Modules
+import Logo from '../3_modules/Logo';
+
+
+export default class Nav_auth extends Component {
+    render() {
+        return (
+            <nav>
+                <Logo />
+            </nav>
+        )
+    }
+}
---
+++ src/components/3_modules/LoginChange.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class LoginChange extends Component {
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
