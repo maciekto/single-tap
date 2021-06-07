8f4ef4bfb56a6d5d2c561f352bfe6fca2b2392ea
maciekto <ekstar007@gmail.com>
2021-06-07 18:28:09 +0200

    Trying to figure out how to do cropping image

--- src/components/2_layouts/ProfilePage.js
+++ src/components/2_layouts/ProfilePage.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import ReactCrop from "react-image-crop";
+import Cropper from 'react-easy-crop'
 
 // assets
 import defaultImage from '../../assets/defaultImage.png'
@@ -9,87 +9,16 @@ import userEditAddPhotoImage from '../../assets/userEditAddPhotoImage.png'
 import ProfilePopup from '../3_modules/PopupProfile'
 class ProfilePage extends Component {
     state = {
+        loaded: false,
         src: null,
         crop: {
-          unit: "%",
-          width: 30,
-          aspect: 1 / 1
-        }
+            x: 0,
+            y: 0
+        },
+        aspect: 1 / 1,
+        zoom: null,
     }
     // IMAGE CROP
-
-    onSelectFile = (e) => {
-        if (e.target.files && e.target.files.length > 0) {
-          const reader = new FileReader();
-          reader.addEventListener("load", () =>
-            this.setState({ src: reader.result })
-          );
-          reader.readAsDataURL(e.target.files[0]);
-        }
-      };
-    // If you setState the crop in here you should return false.
-    onImageLoaded = (image) => {
-        this.imageRef = image;
-    };
-
-    onCropComplete = (crop) => {
-        this.makeClientCrop(crop);
-    };
-
-    onCropChange = (crop, percentCrop) => {
-        // You could also use percentCrop:
-        // this.setState({ crop: percentCrop });
-        this.setState({ crop });
-    };
-
-    async makeClientCrop(crop) {
-        if (this.imageRef && crop.width && crop.height) {
-        const croppedImageUrl = await this.getCroppedImg(
-            this.imageRef,
-            crop,
-            "newFile.jpeg"
-        );
-        this.setState({ croppedImageUrl });
-        }
-    }
-
-    getCroppedImg(image, crop, fileName) {
-        const canvas = document.createElement("canvas");
-        const scaleX = image.naturalWidth / image.width;
-        const scaleY = image.naturalHeight / image.height;
-        canvas.width = crop.width;
-        canvas.height = crop.height;
-        const ctx = canvas.getContext("2d");
-
-        ctx.drawImage(
-        image,
-        crop.x * scaleX,
-        crop.y * scaleY,
-        crop.width * scaleX,
-        crop.height * scaleY,
-        0,
-        0,
-        crop.width,
-        crop.height
-        );
-
-        return new Promise((resolve, reject) => {
-        canvas.toBlob((blob) => {
-            if (!blob) {
-            //reject(new Error('Canvas is empty'));
-            console.error("Canvas is empty");
-            return;
-            }
-            blob.name = fileName;
-            window.URL.revokeObjectURL(this.fileUrl);
-            this.fileUrl = window.URL.createObjectURL(blob);
-            resolve(this.fileUrl);
-        }, "image/jpeg");
-        });
-    }
-
-
-
     componentDidMount = () => {
         console.log(this.props.user)
     }
@@ -97,30 +26,56 @@ class ProfilePage extends Component {
     openPopup = () => {
         console.log('EDIT THAT SHIEET')
     }
+    onSelectFile = (e) => {
+        const reader = new FileReader();
 
+        reader.addEventListener('load', function() {
+            console.log('siemano')
+            this.setState({
+                src: reader.result,
+                loaded: true
+            })
+        }, false)
+    }
+
+
+    onCropChange = (crop) => {
+        this.setState({
+            crop: crop
+        })
+    }
+    onZoomChange = (zoom) => {
+        this.setState({ zoom })
+    }
+    onCropComplete = (croppedArea, croppedAreaPixels) => {
+        console.log(croppedArea, croppedAreaPixels)
+    }
     render() {
-        const { crop, croppedImageUrl, src } = this.state;
         
         return (
 
             <div className='ProfilePage'>
                 <div className='ProfilePage-UserImage'>
                     <img className='ProfilePage-UserImage-Photo' src={this.props.user.photoURL !== null ? this.props.user.photoURL : defaultImage} />
-                    <div className='ProfilePage-UserImage-AddIcon'>
-                        <img className='ProfilePage-UserImage-AddIcon-Image' src={userEditAddPhotoImage} />
-                        <input type="file" accept="image/*" onChange={this.onSelectFile}></input>
-                    </div>
+                    <label htmlFor='ImageInput' className='ProfilePage-UserImage-AddIcon'>
+                        
+                            <img className='ProfilePage-UserImage-AddIcon-Image' src={userEditAddPhotoImage}/>
+                        
+                        <input id='ImageInput' type="file" accept="image/*" onChange={this.onSelectFile} style={{display: 'none', width: '0px', height: '0px'}}></input>
+                    </label>
                 </div>
-                {src && (
-                    <ReactCrop
-                        src={src}
-                        crop={crop}
-                        ruleOfThirds
-                        onImageLoaded={this.onImageLoaded}
-                        onComplete={this.onCropComplete}
-                        onChange={this.onCropChange}
-                    />
-                    )}
+                {
+                    this.state.loaded === true ? <Cropper
+                    image={this.state.src}
+                    crop={this.state.crop}
+                    zoom={this.state.zoom}
+                    aspect={this.state.aspect}
+                    onCropChange={this.onCropChange}
+                    onCropComplete={this.onCropComplete}
+                    onZoomChange={this.onZoomChange}
+                /> : null
+                }
+                
                 <div className='ProfilePage-TitleName ProfilePage-Title'>
                     Name:
                 </div>
