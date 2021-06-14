247375a7fd0168861718fa34c2c504f1c3b28e3a
maciekto <ekstar007@gmail.com>
2021-06-14 21:08:09 +0200

    Start Profile Popup

--- src/components/2_layouts/ProfilePage.js
+++ src/components/2_layouts/ProfilePage.js
@@ -6,9 +6,11 @@ import defaultImage from '../../assets/defaultImage.png'
 import userEditAddPhotoImage from '../../assets/userEditAddPhotoImage.png'
 
 // Modules
-import ProfilePopup from '../3_modules/PopupProfile'
+import PopupProfile from '../3_modules/PopupProfile'
 class ProfilePage extends Component {
     state = {
+        popup: null,
+        file: null,
         loaded: false,
         src: null,
         crop: {
@@ -20,90 +22,107 @@ class ProfilePage extends Component {
     }
     // IMAGE CROP
     componentDidMount = () => {
-        console.log(this.props.user)
+        console.log(this.props.user);
     }
 
-    openPopup = () => {
-        console.log('EDIT THAT SHIEET')
+    openPopup = (type) => {
+        switch(type) {
+            case 'Name':
+                this.setState({
+                    popup: <PopupProfile 
+                        editType={type}
+                    />
+                })
+                break;
+            case 'Surename':
+                this.setState({
+                    popup: <PopupProfile 
+                        editType={type}
+                    />
+                })
+                break;
+            case 'Password':
+                this.setState({
+                    popup: <PopupProfile 
+                        editType={type}
+                    />
+                })
+                break;
+            default:
+                console.error('ERROR')
+        }
     }
     readFile = (e) => {
         this.setState({
             loaded: true,
             src: e.target.result
         })
-        console.log(e.target.result)
+        console.log(e.target.result);
     }
     onSelectFile = (e) => {
-        let file = e.target.files[0]
-        const reader = new FileReader();
-        reader.addEventListener('load', this.readFile);
-        reader.readAsText(file);
-        console.log('LOADING FILE...')
-    }
-
-
-    onCropChange = (crop) => {
+        let file = e.target.files[0];
         this.setState({
-            crop: crop
+            file: file
         })
+        console.log(file)
     }
-    onZoomChange = (zoom) => {
-        this.setState({ zoom })
-    }
-    onCropComplete = (croppedArea, croppedAreaPixels) => {
-        console.log(croppedArea, croppedAreaPixels)
-    }
+
+
+    // onCropChange = (crop) => {
+    //     this.setState({
+    //         crop: crop
+    //     })
+    // }
+    // onZoomChange = (zoom) => {
+    //     this.setState({ zoom })
+    // }
+    // onCropComplete = (croppedArea, croppedAreaPixels) => {
+    //     console.log(croppedArea, croppedAreaPixels);
+    // }
     render() {
         
         return (
-
-            <div className='ProfilePage'>
-                <div className='ProfilePage-UserImage'>
-                    <img className='ProfilePage-UserImage-Photo' src={this.props.user.photoURL !== null ? this.props.user.photoURL : defaultImage} />
-                    <label htmlFor='ImageInput' className='ProfilePage-UserImage-AddIcon'>
-                        
-                            <img className='ProfilePage-UserImage-AddIcon-Image' src={userEditAddPhotoImage}/>
-                        
-                        <input id='ImageInput' type="file" accept="image/*" onChange={this.onSelectFile} style={{display: 'none', width: '0px', height: '0px'}}></input>
-                    </label>
+            <>
+                <div className='ProfilePage'>
+                    <div className='ProfilePage-UserImage'>
+                        <img className='ProfilePage-UserImage-Photo' src={this.props.user.photoURL !== null ? this.props.user.photoURL : defaultImage} />
+                        <label htmlFor='ImageInput' className='ProfilePage-UserImage-AddIcon'>
+                            
+                                <img className='ProfilePage-UserImage-AddIcon-Image' src={userEditAddPhotoImage}/>
+                            
+                            <input id='ImageInput' type="file" accept="image/*" onChange={this.onSelectFile} style={{display: 'none', width: '0px', height: '0px'}}></input>
+                        </label>
+                    </div>
+                    <div className='ProfilePage-TitleName ProfilePage-Title'>
+                        Name:
+                    </div>
+                    <div className='ProfilePage-DataName ProfilePage-Data'>
+                        {this.props.user.displayName}
+                    </div>
+                    <div className='ProfilePage-EditName ProfilePage-Edit' onClick={() => this.openPopup('Name')}>
+                        Edit
+                    </div>
+                    <div className='ProfilePage-TitleEmail ProfilePage-Title'>
+                        E-mail:
+                    </div>
+                    <div className='ProfilePage-DataEmail ProfilePage-Data'>
+                        {this.props.user.email}
+                    </div>
+                    <div className='ProfilePage-EditEmail ProfilePage-Edit' onClick={() => this.openPopup('Surename')}>
+                        Edit
+                    </div>
+                    <div className='ProfilePage-TitlePassword ProfilePage-Title'>
+                        Password:
+                    </div>
+                    <div className='ProfilePage-DataPassword ProfilePage-Data'>
+                        ********
+                    </div>
+                    <div className='ProfilePage-EditPassword ProfilePage-Edit' onClick={() => this.openPopup('Password')}>
+                        Edit
+                    </div>
                 </div>
-                    <Cropper
-                        image={this.state.src}
-                        crop={this.state.crop}
-                        zoom={this.state.zoom}
-                        aspect={this.state.aspect}
-                        onCropChange={this.onCropChange}
-                        onCropComplete={this.onCropComplete}
-                        onZoomChange={this.onZoomChange}
-                    />
-                <div className='ProfilePage-TitleName ProfilePage-Title'>
-                    Name:
-                </div>
-                <div className='ProfilePage-DataName ProfilePage-Data'>
-                    {this.props.user.displayName}
-                </div>
-                <div className='ProfilePage-EditName ProfilePage-Edit' onClick={this.openPopup}>
-                    Edit
-                </div>
-                <div className='ProfilePage-TitleEmail ProfilePage-Title'>
-                    E-mail:
-                </div>
-                <div className='ProfilePage-DataEmail ProfilePage-Data'>
-                    {this.props.user.email}
-                </div>
-                <div className='ProfilePage-EditEmail ProfilePage-Edit'>
-                    Edit
-                </div>
-                <div className='ProfilePage-TitlePassword ProfilePage-Title'>
-                    Password:
-                </div>
-                <div className='ProfilePage-DataPassword ProfilePage-Data'>
-                    ********
-                </div>
-                <div className='ProfilePage-EditPassword ProfilePage-Edit'>
-                    Edit
-                </div>
-            </div>
+                {this.state.popup}
+            </>
         )
     }
 }
--- src/components/3_modules/PopupProfile.js
+++ src/components/3_modules/PopupProfile.js
@@ -3,21 +3,21 @@ import React, { Component } from 'react'
 export default class PopupProfile extends Component {
     render() {
         return (
-            <div className='ProfilePopup'>
-                <div className='ProfilePopup-Main'>
-                    <div className='ProfilePopup-Title'>
-                        Edit this.props.editType
+            <div className='PopupProfile'>
+                <div className='PopupProfile-Main'>
+                    <div className='PopupProfile-Title'>
+                        Edit {this.props.editType}
                     </div>
-                    <div className='ProfilePopup-Label'>
-                        this.propss.editType
+                    <div className='PopupProfile-Label'>
+                        {this.props.editType}
                     </div>
-                    <div className='ProfilePopup-Input'>
+                    <div className='PopupProfile-Input'>
                         <input type='text' defaultValue={null} />
                     </div>
-                    <div className='ProfilePopup-Back'>
+                    <div className='PopupProfile-Back'>
                         Back
                     </div>
-                    <div className='ProfilePopup-Save'>
+                    <div className='PopupProfile-Save'>
                         Save
                     </div>
                 </div>
